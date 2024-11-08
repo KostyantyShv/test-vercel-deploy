bc9762ec9b9f06c0b0092333655a6a5b3f1f614f
kostyantyn <blackmirror.ks10@gmail.com>
2024-11-08 11:12:44 +0200

    new components structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import './App.css'
-import { ClerkProvider } from '@clerk/clerk-react'
 import useSWR from 'swr'
 
 const fetcher = (url: string) => fetch(url).then(res => res.json())
@@ -11,15 +10,13 @@ function App() {
   if (isLoading) return <div>Loading...</div>
 
   return (
-    <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
-      <div>
-        <h1>{data.name}</h1>
-        <p>{data.description}</p>
-        <strong>👁 {data.subscribers_count}</strong>{' '}
-        <strong>✨ {data.stargazers_count}</strong>{' '}
-        <strong>🍴 {data.forks_count}</strong>
-      </div>
-    </ClerkProvider>
+    <div>
+      <h1>{data.name}</h1>
+      <p>{data.description}</p>
+      <strong>👁 {data.subscribers_count}</strong>{' '}
+      <strong>✨ {data.stargazers_count}</strong>{' '}
+      <strong>🍴 {data.forks_count}</strong>
+    </div>
   )
 }
 
