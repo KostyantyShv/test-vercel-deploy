f93b00862bc6898cffbaf6e45fdbbfd9ea456f53
kostyantyn <blackmirror.ks10@gmail.com>
2024-11-08 13:35:56 +0200

    direct swix using

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react-swc"
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      injectRegister: 'script-defer',
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      manifest: false,
+      useCredentials: true
+    })
+  ],
   build: {
     sourcemap: true,
     minify: 'esbuild',
