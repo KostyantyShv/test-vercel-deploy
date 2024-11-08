f31194d0f1fc5dd96500a139d4a4e5c35a6a85db
kostyantyn <blackmirror.ks10@gmail.com>
2024-11-08 10:44:12 +0200

    init

---
+++ test-vercel-deploy/src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import useSWR from 'swr'
+
+const fetcher = (url: string) => fetch(url).then(res => res.json())
+
+function App() {
+  const { data, error, isLoading } = useSWR('https://api.github.com/repos/vercel/swr', fetcher)
+
+  if (error) return <div>Failed to load</div>
+  if (isLoading) return <div>Loading...</div>
+
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <p>{data.description}</p>
+      <strong>👁 {data.subscribers_count}</strong>{' '}
+      <strong>✨ {data.stargazers_count}</strong>{' '}
+      <strong>🍴 {data.forks_count}</strong>
+    </div>
+  )
+}
+
+export default App
